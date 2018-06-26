07f04331247c8e2b8a2910b1fa0931d03395ff91
Scott Wyngarden <swyngarden@gmail.com>
2018-06-26 00:40:30 -0700

    adding shell of tests

---
+++ spec/requests/api_developments_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "ApiDevelopments", type: :request do
+  describe "RDBMS-backed" do
+    it "create RDBMS-backed model"
+    it "create RDBMS-backed API"
+  end
+
+  describe "Mongo-backed" do
+    it "create Mongo-backed model"
+    it "create Mongo-backed API"
+  end
+
+
+end
