ca6cb50773f2702fedc17f416bbd66743cd56011
Scott Wyngarden <swyngarden@gmail.com>
2018-06-26 10:33:58 -0700

    adding mongo tests, and adding Foo via rails-api g scaffold

--- spec/requests/api_developments_spec.rb
+++ spec/requests/api_developments_spec.rb
@@ -1,14 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe "ApiDevelopments", type: :request do
+
+  def parsed_body
+    JSON.parse(response.body)
+  end
+
   describe "RDBMS-backed" do
-    it "create RDBMS-backed model"
-    it "create RDBMS-backed API"
+    before(:each) { Foo.delete_all}
+    after(:each) { Foo.delete_all}
+    it "create RDBMS-backed model" do
+      foo = Foo.create(:name => "test")
+      expect(Foo.find(foo.id).name).to eq("test")
+    end
+
+    it "expose API resourses under '/api'" do
+      expect(foos_path).to eq("/api/foos")
+    end
+
+    it "create RDBMS-backed API" do
+      foo = Foo.create(:name => "test")
+      get foo_path(object.id)
+      expect(response).to have_http_status(:ok)
+      expect(parsed_body["name"]).to eq("test")
+    end
   end
 
   describe "Mongo-backed" do
-    it "create Mongo-backed model"
-    it "create Mongo-backed API"
+    before(:each) { Bar.delete_all}
+    after(:each) { Bar.delete_all}
+    it "create Mongo-backed model" do
+      bar = Bar.create(:name => "test")
+      expect(bar.find(bar.id).name).to eq("test")
+    end
+
+    it "expose API resourses under '/api'" do
+      expect(bars_path).to eq("/api/bars")
+    end
+
+    it "create Mongo-backed API" do
+      bar = Bar.create(:name => "test")
+      get bar_path(object.id)
+      expect(response).to have_http_status(:ok)
+      expect(parsed_body["name"]).to eq("test")
+    end
   end
 
 
